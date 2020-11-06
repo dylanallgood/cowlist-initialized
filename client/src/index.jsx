e0b92758b9bcf70ed4201db15b3a3a66c863011c
dylanallgood <dylanblakeallgood@gmail.com>
2020-11-06 15:09:22 -0500

    init

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render() {
-    return (
-    <div>Hello World!</div>
-    );
+    return <div>Hello World!</div>;
   }
 }
 
-var mountNode = document.getElementById("app");
-ReactDOM.render(<App/>, mountNode);
+ReactDOM.render(<App />, document.getElementById('app'));
