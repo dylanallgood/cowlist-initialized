51ca02c4ff645dd762d16227f19028bd4e22b290
dylanallgood <dylanblakeallgood@gmail.com>
2020-11-06 22:22:09 -0500

    updated api request

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import List from './components/List';
+import Search from './components/Search';
+import axios from 'axios';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cowList: [],
+    };
+  }
+
+  componentDidMount() {
+    this.handleSearch();
+  }
+
+  handleSearch() {
+    axios.get(`http://127.0.0.1:3000/api/cows`).then((res) => {
+      let cows = res.data;
+      this.setState({ cowList: cows });
+    });
+  }
+
   render() {
-    return <div>Hello World!</div>;
+    return (
+      <div>
+        <List cowList={this.state.cowList} />
+        <Search search={this.handleSearch} />
+      </div>
+    );
   }
 }
 
